a28ac9727ecd26c1b371f4e98b9e541db49b408f
pakized <72470944+pakized@users.noreply.github.com>
2021-12-25 22:16:10 +0100

    add the code part seperately
    add the code I changed in a seperate file

--- FilesIChanged/playground.cpp
+++ FilesIChanged/playground.cpp
@@ -4,6 +4,9 @@
 #include <glfw3.h>
 GLFWwindow* window;
 
+//https://stackoverflow.com/questions/50975196/is-it-possible-to-change-the-color-of-an-object-with-a-variable-in-opengl
+//change color
+
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,21 +32,95 @@ int main( void )
   bool vertexbufferInitialized = initializeVertexbuffer();
   if (!vertexbufferInitialized) return -1;
 
+  //brauche ich für jedes object einen vertex buffer die restlichen objekte bewegen sich gleich haben 
+  // aber einen anderen start punkt
+  //neuer code: ( project does not work with this)
+  bool vertexbufferForObjectAInitialized = initializeVertexbufferForObjectA();
+  if (!vertexbufferForObjectAInitialized) return -1;
+
+
+  //color
+  rS = 0.0;
+  gS = 1.0;
+  bS = 1.0;
+
+  rB = 1.0;
+  gB = 0.0;
+  bB = 1.0;
+
+  rSt = 0.0;
+  gSt = 0.0;
+  bSt = 0.0;
+
+
+  bool vertexbufferForObjectBInitialized = initializeVertexbufferForObjectB();
+  if (!vertexbufferForObjectBInitialized) return -1;
+
+  bool vertexbufferForObjectCInitialized = initializeVertexbufferForObjectC();
+  if (!vertexbufferForObjectCInitialized) return -1;
+
+ /* bool vertexbufferForObjectB = initializeVertexbuffer();
+  if (!vertexbufferForObjectB) return -1;
+
+  bool vertexbufferForObjectC = initializeVertexbuffer();
+  if (!vertexbufferForObjectC) return -1;*/
+
+  //vertexbuffer: VBO stores the vertex data (triangle vertices)
+  //                VBO is an array of data (mostly floats)
+  // copy all data in a buffer so we dont have to draw one by one 
+  //buffer stores data in the graphics hardware memory
+  //= more efficient to use gpu
+
+
   // Create and compile our GLSL program from the shaders
   programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+  //this creates an GLSL program for the shaders so we can use them
+
+  //brauch ich mehr shader wenn models verschieden bewegen will
 
   // Get a handle for our "MVP" and "MV" uniforms and update them for initalization 
   MatrixIDMV = glGetUniformLocation(programID, "MV");
+
+  //parameters of glGetUniformLocation(Program object, *name: points to a string the name of the uniform variables)
+    //returns integer that represents the location of a specific uniform variable within a program object
+  // name must be in an active 
+  // => returns programID?
+
+  //send transformation to currently bound shader 
+  //each model will have different mvp matrixes 
   MatrixID = glGetUniformLocation(programID, "MVP");
-  updateMVPTransformation();
+
+
+  //function to move 
+  updateMVPTransformation(0,0,0);
 
   //initialize pose variables
   curr_x = 0;
   curr_y = 0;
   curr_angle = 0;
+  int timer = 0;
 
+  //neuer code:
+  //position second object Done
+  xA = 25.5f;
+  yA = 0.0f;
+  zA = 0.0f;
+
+  //position third object Done
+  xB = -135.5f;
+  yB = 25.0f;
+  zB = 0.0f;
+
+  //position fourth object  TODO
+  xC = -0.0f;
+  yC = 0.0f;
+  zC = 0.0f;
+  //bis hier
+  // 
   // Enable depth test
   glEnable(GL_DEPTH_TEST);
+  //needed to store fragments their z valuee in depht buffer => is used clear depht buffer before each 
+                                                                    //frame by GL_Depth_buffer_bit
 
 	//start animation loop until escape key is pressed
 	do{
@@ -63,6 +140,8 @@ int main( void )
 	return 0;
 }
 
+
+
 void updateAnimationLoop()
 {
   // Clear the screen
@@ -71,15 +150,86 @@ void updateAnimationLoop()
   // Use our shader
   glUseProgram(programID);
 
+ 
+  GLint myUniformLocation = glGetUniformLocation(programID, "rgbColor");
+  glUniform3f(myUniformLocation, rB, gB, bB)/* some floating point value here */;
+
+
   // Update the variables for movement / rotation if a key was pressed
-  if (glfwGetKey(window, GLFW_KEY_W)) curr_y+=1.0;
-  else if (glfwGetKey(window, GLFW_KEY_S)) curr_y-=1.0;
-  else if (glfwGetKey(window, GLFW_KEY_A)) curr_x-=1.0;
-  else if (glfwGetKey(window, GLFW_KEY_D)) curr_x+=1.0;
-  else if (glfwGetKey(window, GLFW_KEY_R)) curr_angle += 0.1;
+  if (glfwGetKey(window, GLFW_KEY_W)) curr_y+=2.0;
+  else if (glfwGetKey(window, GLFW_KEY_S)) curr_y-=2.0;
+  else if (glfwGetKey(window, GLFW_KEY_A)) curr_x-=2.0;
+  else if (glfwGetKey(window, GLFW_KEY_D)) curr_x+=2.0;
+  //else if (glfwGetKey(window, GLFW_KEY_R)) curr_angle += 0.5;
+
+  bool dangerZone = false;
+
+  if (curr_x <= xB + 50 || curr_x >= xA + 75 || curr_y < 0)             //-135.5f; = xA add number for bunny head when its in the danger zone get red
+  {
+      rB = 1.0;
+      gB = 0.0;
+      bB = 0.0;
+      dangerZone = true;
+  }
+  if (curr_x > xB + 50 && curr_x < xA + 75 && curr_y > 0)             //change color back to normal when bunny is out of danger zone
+  {
+      rB = 1.0;
+      gB = 0.0;
+      bB = 1.0;
+      dangerZone = false;
+  }
+  if (curr_y > 99 && dangerZone == false)               //change color when bunny is at a save spot
+  {
+      rB = 0.0;
+      gB = 1.0;
+      bB = 0.7;
+  }
+
+  timer++;
+  if (timer >= 5) {
+  //change color:
+  rSt = rSt + 0.1;
+  if (rSt >= 1) 
+  {
+      rSt = 0.5;
+  }
+  gSt = gSt + 0.2;
+  if (gSt >= 1)
+  {
+      gSt = 0.5;
+  }  
+  bSt = bSt + 0.3;
+  if (bSt >= 1)
+  {
+      bSt = 0.5;
+  }
+  timer = 0;
+  }
+
+  //for the other models it would be:
+  /*
+        y = y-1  (only from above to bottom then)
+        if y = end window 
+            delete model add new model with the same start points 
+            or change change ý value of the model = same effect
+  */
+
+  yA = yA - 1;
+  if (yA <= -300) 
+  {
+      yA = 0;
+  }
+
+  yB = yB - 1.7;
+  if (yB <= -350)
+  {
+      yB = 0;
+  }
+
+  curr_angle += 0.05;
 
   // Update the MVP transformation with the new values
-  updateMVPTransformation();
+  updateMVPTransformation(curr_x, curr_y, 0.0);
 
   // Send our transformation to the currently bound shader, 
   // in the "MVP" uniform and also the "MV" uniform
@@ -110,17 +260,152 @@ void updateAnimationLoop()
     (void*)0            // array buffer offset
   );
 
-  // Draw the triangle !
+  // Draw the triangle ! fishA
   glDrawArrays(GL_TRIANGLES, 0, vertexbuffer_size); // 3 indices starting at 0 -> 1 triangle
+  updateMVPTransformation(xA, yA, 0);
+  glUniform3f(myUniformLocation, rS, gS, bS)/* some floating point value here */;
+
+  //  updateMVPTransformation(xA + 1, yA +1 , 0 + 1);
+
+
+  //new code (copy down the code from above for our next vertex buffer):
+  // 
+    // Send our transformation to the currently bound shader, 
+  // in the "MVP" uniform and also the "MV" uniform
+  glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+  glUniformMatrix4fv(MatrixIDMV, 1, GL_FALSE, &MV[0][0]);
+
+  // 1rst attribute buffer : vertices
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectA[0]);
+
+  //bind buffer (array buffer) to buffer binding point (vertexbuffer)
+  //bind name (2.parameter) to target(1.parameter)
+
+  glVertexAttribPointer(
+      0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+
+  // 2nd attribute buffer : normals
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectA[1]);
+  glVertexAttribPointer(
+      1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+
+  //color?
+  // 
+  // 
+
+
+  // Draw the triangle ! //fish
+  glDrawArrays(GL_TRIANGLES, 0, vertexbufferForObjectA_size); // 3 indices starting at 0 -> 1 triangle
+  updateMVPTransformation(xB, yB, 0);
+
+  //new code (copy down the code from above for our next vertex buffer):
+  // 
+    // Send our transformation to the currently bound shader, 
+  // in the "MVP" uniform and also the "MV" uniform
+  glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+  glUniformMatrix4fv(MatrixIDMV, 1, GL_FALSE, &MV[0][0]);
+
+  // 1rst attribute buffer : vertices
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectB[0]);
+
+  //bind buffer (array buffer) to buffer binding point (vertexbuffer)
+  //bind name (2.parameter) to target(1.parameter)
+
+  glVertexAttribPointer(
+      0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+
+  // 2nd attribute buffer : normals
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectB[1]);
+  glVertexAttribPointer(
+      1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+  // Draw the triangle ! //fish
+  glDrawArrays(GL_TRIANGLES, 0, vertexbufferForObjectB_size); // 3 indices starting at 0 -> 1 triangle
+
+
+
+  updateMVPTransformation(xC, yC, curr_angle);
+  glUniform3f(myUniformLocation, rSt, gSt, bSt)/* some floating point value here */;
+
+  //new code (copy down the code from above for our next vertex buffer):
+  // 
+    // Send our transformation to the currently bound shader, 
+  // in the "MVP" uniform and also the "MV" uniform
+  glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+  glUniformMatrix4fv(MatrixIDMV, 1, GL_FALSE, &MV[0][0]);
+
+  // 1rst attribute buffer : vertices
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectC[0]);
+
+  //bind buffer (array buffer) to buffer binding point (vertexbuffer)
+  //bind name (2.parameter) to target(1.parameter)
+
+  glVertexAttribPointer(
+      0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+
+  // 2nd attribute buffer : normals
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectC[1]);
+  glVertexAttribPointer(
+      1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+  );
+  // Draw the triangle ! //fish
+  glDrawArrays(GL_TRIANGLES, 0, vertexbufferForObjectC_size); // 3 indices starting at 0 -> 1 triangle
+
+
+
+
+  //maybee copy this 2 more times 
+  //end code
 
   // Swap buffers
   glfwSwapBuffers(window);
   glfwPollEvents();
 }
 
+//no need to change something
 bool initializeWindow()
 {
-  // Initialise GLFW
+  // Initialise GLFW test
   if (!glfwInit())
   {
     fprintf(stderr, "Failed to initialize GLFW\n");
@@ -161,9 +446,9 @@ bool initializeWindow()
   return true;
 }
 
-bool updateMVPTransformation()
+bool updateMVPTransformation(float posX, float posY, float angle)
 {
-  // Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+  // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
   glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 10000.0f);
 
   // Camera matrix (modify this to let the camera move)
@@ -175,8 +460,9 @@ bool updateMVPTransformation()
 
   // Model matrix, example with 3 parameters (curr_angle, curr_x, curr_y)
   glm::mat4 Model = glm::mat4(1.0f); //start with identity matrix
-  Model = glm::rotate(Model, curr_angle, glm::vec3(1.0f, 0.0f, 0.0f)); //apply orientation (last parameter: axis)
-  Model = glm::translate(Model,glm::vec3(curr_x, curr_y, 0.0f)); //apply translation
+  //change rotation:
+  Model = glm::rotate(Model, angle, glm::vec3(-0.0f, -1.0f, -0.50f)); //apply orientation (last parameter: axis)
+  Model = glm::translate(Model,glm::vec3(posX, posY, 0.0f)); //apply translation
   
   // Our ModelViewProjection : multiplication of our 3 matrices
   MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
@@ -193,7 +479,12 @@ bool initializeVertexbuffer()
   //create vertex and normal data
   std::vector< glm::vec3 > vertices = std::vector< glm::vec3 >();
   std::vector< glm::vec3 > normals = std::vector< glm::vec3 >();
-  loadSTLFile(vertices, normals, "C:/temp/bunny-decimated.stl");
+
+  //std::vector< glm::vec3 > vertices2 = std::vector< glm::vec3 >();
+  //std::vector< glm::vec3 > normals2 = std::vector< glm::vec3 >();
+
+  loadSTLFile(vertices, normals, "C:/temp/bunny-decimated.stl", 0, 0);
+  //loadSTLFile(vertices2, normals2, "C:/temp/Shark.stl");
   vertexbuffer_size = vertices.size() * sizeof(glm::vec3);
   
   glGenBuffers(2, vertexbuffer); //generate two buffers, one for the vertices, one for the normals
@@ -213,10 +504,134 @@ bool initializeVertexbuffer()
   return true;
 }
 
+
+//again some code (I hope this works)
+//we have to initilaize our other0 Vbuffer
+bool initializeVertexbufferForObjectA()
+{
+    /*zum bewegen glm:mat4 = model matrix
+    * model = einheitsmatrix
+    * matrix als uniform in shader vertexshader
+    * mat4 u_model;
+    * u_model + vec4(position)
+    * unifrm location abfragen 
+    * int modelMatrixLocation = GLCALL(glGetUniformLocation(shader.getshaderId(), u_model));
+    */
+    glGenVertexArrays(1, &VertexArrayForObjectAID);
+    glBindVertexArray(VertexArrayForObjectAID);
+
+    //create vertex and normal data
+    //std::vector< glm::vec3 > vertices = std::vector< glm::vec3 >();
+    //std::vector< glm::vec3 > normals = std::vector< glm::vec3 >();
+
+    std::vector< glm::vec3 > vertices= std::vector< glm::vec3 >();
+    std::vector< glm::vec3 > normals= std::vector< glm::vec3 >();
+
+    glm::vec3 triangleNormal = glm::vec3(xA,
+        yA,
+        zA);
+
+    vertices.push_back(vec3(xA,yA,0.0));
+    normals.push_back(triangleNormal);
+
+    loadSTLFile(vertices, normals, "C:/temp/Shark.stl", xA, yA);
+    vertexbufferForObjectA_size = vertices.size() * sizeof(glm::vec3);
+
+    glGenBuffers(2, vertexbufferForObjectA); //generate two buffers, one for the vertices, one for the normals
+
+    //glGenBuffers(n, buffers) n = number of buffers, buffers = array in which buffer object names are stored 
+
+    //fill first buffer (vertices)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectA[0]);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    //fill second buffer (normals)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectA[1]);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    return true;
+}
+
+
+//we have to initilaize our other0 Vbuffer
+bool initializeVertexbufferForObjectB()
+{
+
+    glGenVertexArrays(1, &VertexArrayForObjectBID);
+    glBindVertexArray(VertexArrayForObjectBID);
+
+    std::vector< glm::vec3 > vertices = std::vector< glm::vec3 >();
+    std::vector< glm::vec3 > normals = std::vector< glm::vec3 >();
+
+
+    loadSTLFile(vertices, normals, "C:/temp/Shark.stl", -100, 0);
+    vertexbufferForObjectB_size = vertices.size() * sizeof(glm::vec3);
+
+    glGenBuffers(2, vertexbufferForObjectB); //generate two buffers, one for the vertices, one for the normals
+
+    //glGenBuffers(n, buffers) n = number of buffers, buffers = array in which buffer object names are stored 
+
+    //fill first buffer (vertices)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectB[0]);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    //fill second buffer (normals)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectB[1]);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    return true;
+}
+
+
+bool initializeVertexbufferForObjectC()
+{
+
+    glGenVertexArrays(1, &VertexArrayForObjectCID);
+    glBindVertexArray(VertexArrayForObjectCID);
+
+    std::vector< glm::vec3 > vertices = std::vector< glm::vec3 >();
+    std::vector< glm::vec3 > normals = std::vector< glm::vec3 >();
+
+
+    loadSTLFile(vertices, normals, "C:/temp/star.stl", 0, -100);
+    vertexbufferForObjectC_size = vertices.size() * sizeof(glm::vec3);
+
+    glGenBuffers(2, vertexbufferForObjectC); //generate two buffers, one for the vertices, one for the normals
+
+    //glGenBuffers(n, buffers) n = number of buffers, buffers = array in which buffer object names are stored 
+
+    //fill first buffer (vertices)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectC[0]);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    //fill second buffer (normals)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbufferForObjectC[1]);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    return true;
+}
+
+
+
+
 bool cleanupVertexbuffer()
 {
   // Cleanup VBO
   glDeleteVertexArrays(1, &VertexArrayID);
+  glDeleteVertexArrays(1, &VertexArrayForObjectAID);
+  glDeleteVertexArrays(1, &VertexArrayForObjectBID);
   return true;
 }
 
@@ -228,7 +643,9 @@ bool closeWindow()
 
 void loadSTLFile(std::vector< glm::vec3 >& vertices, 
                  std::vector< glm::vec3 >& normals, 
-                 std::string stl_file_name)
+                 std::string stl_file_name,
+                 float startPointX,
+                 float startPointY)
 {
   stl::stl_data info = stl::parse_stl(stl_file_name);
   std::vector<stl::triangle> triangles = info.triangles;
@@ -238,13 +655,13 @@ void loadSTLFile(std::vector< glm::vec3 >& vertices,
                                          t.normal.y, 
                                          t.normal.z);
     //add vertex and normal for point 1:
-    vertices.push_back(glm::vec3(t.v1.x, t.v1.y, t.v1.z));
+    vertices.push_back(glm::vec3(t.v1.x + startPointX, t.v1.y + startPointY, t.v1.z));
     normals.push_back(triangleNormal);
     //add vertex and normal for point 2:
-    vertices.push_back(glm::vec3(t.v2.x, t.v2.y, t.v2.z));
+    vertices.push_back(glm::vec3(t.v2.x + startPointX, t.v2.y + startPointY, t.v2.z));
     normals.push_back(triangleNormal);
     //add vertex and normal for point 3:
-    vertices.push_back(glm::vec3(t.v3.x, t.v3.y, t.v3.z));
+    vertices.push_back(glm::vec3(t.v3.x + startPointX, t.v3.y + startPointY, t.v3.z));
     normals.push_back(triangleNormal);
   }
 }
--- FilesIChanged/playground.h
+++ FilesIChanged/playground.h
@@ -15,6 +15,19 @@ GLuint vertexbuffer[2];
 GLuint VertexArrayID;
 GLuint vertexbuffer_size;
 
+//some global variables for handling the vertex buffer
+GLuint vertexbufferForObjectA[2];
+GLuint VertexArrayForObjectAID;
+GLuint vertexbufferForObjectA_size;
+
+GLuint vertexbufferForObjectB[2];
+GLuint VertexArrayForObjectBID;
+GLuint vertexbufferForObjectB_size;
+
+GLuint vertexbufferForObjectC[2];
+GLuint VertexArrayForObjectCID;
+GLuint vertexbufferForObjectC_size;
+
 //program ID of the shaders, required for handling the shaders with OpenGL
 GLuint programID;
 
@@ -29,11 +42,28 @@ float curr_x;
 float curr_y;
 float curr_angle;
 
+float xA, yA, zA;
+float xC, yC, zC;
+float xB, yB, zB;
+
+float rS, gS, bS;
+float rB, gB, bB;
+float rSt, gSt, bSt;
+
+
+static const GLfloat g_color_buffer_data;
+
+
 int main( void ); //<<< main function, called at startup
 void updateAnimationLoop(); //<<< updates the animation loop
 bool initializeWindow(); //<<< initializes the window using GLFW and GLEW
-bool updateMVPTransformation(); //<<< updates the MVP transform with the current pose
+bool updateMVPTransformation(float x, float y, float angle); //<<< updates the MVP transform with the current pose
 bool initializeVertexbuffer(); //<<< initializes the vertex buffer array and binds it OpenGL
+bool initializeVertexbufferForObjectA(); //<<< initializes the vertex buffer array and binds it OpenGL
+bool initializeVertexbufferForObjectB(); //<<< initializes the vertex buffer array and binds it OpenGL
+bool initializeVertexbufferForObjectC(); //<<< initializes the vertex buffer array and binds it OpenGL
+int timer;
+
 bool cleanupVertexbuffer(); //<<< frees all recources from the vertex buffer
 bool closeWindow(); //<<< Closes the OpenGL window and terminates GLFW
 
@@ -43,6 +73,6 @@ bool closeWindow(); //<<< Closes the OpenGL window and terminates GLFW
  * @param[out] normals   Vector of normals, needs to be empty and is filled by the function.
    @param[in] stl_file_name File name of the STL file that should be loaded
  */
-void loadSTLFile(std::vector< glm::vec3 >& vertices, std::vector< glm::vec3 >& normals, std::string stl_file_name);
+void loadSTLFile(std::vector< glm::vec3 >& vertices, std::vector< glm::vec3 >& normals, std::string stl_file_name, float startX, float startY);
 
 #endif
